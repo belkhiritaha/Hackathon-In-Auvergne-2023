22216ef161f6540ffd0f725ffd730518f56d5cd9
Taha <belkhiri.taha53@gmail.com>
2023-10-14 11:20:41 +0200

    added charts

--- eau/src/App.tsx
+++ eau/src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import SimpleAreaChart from './components/charts/Linechart'
 
 function App() {
     return (
         <>
-            <Navbar /> 
+            <Navbar />
+
+            <SimpleAreaChart /> 
         </>
     )
 }
